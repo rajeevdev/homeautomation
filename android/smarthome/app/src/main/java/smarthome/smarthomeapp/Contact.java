644cc7aafa0967b44d2251b7d171e684153a071b
rajeevdev <rajeev.dev@gmail.com>
2016-03-16 20:52:58 +0530

    Android app added.

---
+++ android/smarthome/app/src/main/java/smarthome/smarthomeapp/Contact.java
@@ -0,0 +1,10 @@
+package smarthome.smarthomeapp;
+
+/**
+ * Created by rade on 15-03-2016.
+ */
+public class Contact {
+    public String surname;
+    public String name;
+    public String email;
+}
